da866cd0fad9ae3d2f26dcc9e88d23fc45ec1346
Jean-Christophe Gueriaud <jean-christophe@vaadin.com>
2022-08-12 14:46:17 +0300

    Add userDetailsService

--- src/main/java/org/vaadin/jchristophe/security/SecurityConfiguration.java
+++ src/main/java/org/vaadin/jchristophe/security/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -18,6 +19,17 @@ public class SecurityConfiguration extends VaadinWebSecurityConfigurerAdapter {
     public static final String LOGOUT_URL = "/logout";
     public static final String LOGOUT_SUCCESS_URL = "/";
 
+    private final UserDetailsService userDetailsService;
+
+    /**
+     * userDetailsService is required for the Remember Me
+     * https://docs.spring.io/spring-security/site/docs/5.0.x/reference/html/remember-me.html
+     * @param userDetailsService
+     */
+    public SecurityConfiguration(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -25,11 +37,10 @@ public class SecurityConfiguration extends VaadinWebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         super.configure(http);
         setLoginView(http, LoginView.class, LOGOUT_SUCCESS_URL);
         String privateSecretKeyToChange = "JKJDSKLDJdJSisdjsdfjmkdjdfkljkJKLjlk";
-        http.rememberMe().key(privateSecretKeyToChange).tokenValiditySeconds(7200);
+        http.rememberMe().key(privateSecretKeyToChange).tokenValiditySeconds(7200).userDetailsService(this.userDetailsService);
         http.logout().logoutRequestMatcher(new AntPathRequestMatcher(LOGOUT_URL, "GET"))
                 .invalidateHttpSession(true)
                 .deleteCookies("JSESSIONID", "remember-me");
